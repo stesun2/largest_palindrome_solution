9ad58e90ea7c70bdb753a9344e5d9c8348475ba6
stesun2 <ss27@illinois.edu>
2021-08-13 11:39:09 -0500

    combined if statements

--- largestPalindrome_stephen.js
+++ largestPalindrome_stephen.js
@@ -1,4 +1,5 @@
-// TODO: Given a positive integer value "N", return the largest numeric palindrome (if one exists) that can be produced by multiplying two N-digit numbers, else return 0 (if one doesn't exist)
+// TODO: Given a positive integer value "N", return the largest numeric palindrome (if one exists) that can be produced
+// by multiplying two N-digit numbers, else return 0 (if one doesn't exist)
 
 // EXAMPLE: N=2, output should be 9009 (91*99)
 
@@ -8,17 +9,21 @@
 //  0. Obtain this file from https://github.com/ankur-cp/largest_palindrome_solution
 //  1. Rename this file, substituting "yourname" with your name
 //  2. Replace "yourname" with your name in the variable below
-exports.name = "yourname";
+exports.name = "stephen";
 //  3. Add your optimizations to the solution below
 //  4. Submit a pull request
 
 
 isPalindrome = (num) => {
+  // console.log(num)
   // determine if input value matches it's reversed value (i.e. check if it's a palindrome!)
   strNum = num.toString();
+  // console.log(strNum.length) 
+  // if (strNum.length == 0){
+  //   return 0;
+  // } 
   return strNum === strNum.split("").reverse().join("");
 }
-
 
 exports.getLargestPalindrome = (N) => {
 
@@ -30,14 +35,15 @@ exports.getLargestPalindrome = (N) => {
     for (let j = 10 ** (N - 1); j < 10 ** N; j++) {
 
       // check if the product is a palindrome
-      if (isPalindrome(i * j)) {
+      if (isPalindrome(i * j) && (i * j > largestPalindrome)) {
+        largestPalindrome = i * j;
 
         // check if it's larger than our largest palindrome
-        if (i * j > largestPalindrome) {
+        // if (i * j > largestPalindrome) {
                   
-          // update largest palindrome
-          largestPalindrome = i * j;
-        }
+        //   // update largest palindrome
+        //   largestPalindrome = i * j;
+        // }
       }
     }
   }
@@ -47,7 +53,7 @@ exports.getLargestPalindrome = (N) => {
 }
 
 // input
-const N = 3;
+const N = 4;
 
 // CHALLENGE: Can you optimize the algorithm above so that works for input values N=4, N=5, N=6 in a "reasonable" amount of time?
 
